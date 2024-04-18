ab28a3486c3e448734c5b18c4233d1fa736de8de
CloverDragana <blaacklist@live.com>
2024-04-18 17:27:13 +0100

    changing navbar to be standardized

--- equipment_management/urls.py
+++ equipment_management/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .import views
+from . import views
 
 urlpatterns = [
-    path ('inventorymanagement', views.equipPage, name='inventorymanagement')
+    path ('', views.equipPage, name='inventorymanagement')
 ]
 
