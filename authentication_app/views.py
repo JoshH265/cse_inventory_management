f48dc35b6d0da21d00647fff21fadc6b7ddecca1
Josh <108278844+JoshH265@users.noreply.github.com>
2024-04-22 18:28:05 +0100

    Editing login/signup page. Admin dashboard changes
    Setup template for admin dashboard
    Continued working on login/signup pages

--- authentication_app/views.py
+++ authentication_app/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render, redirect
 from home.views import homepage # Importing the homepage view from the home app
 
-from . forms import UserCreation # Importing the UserCreation form
+from . forms import UserCreation, UserLogin # Importing the UserCreation form and the signup form
 from django.contrib import messages
+
+
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate, login, logout
 
 def home(request):
     return homepage(request)
 
-def signup(request):
+def sign_up(request):
     form = UserCreation()
     
     if request.method == 'POST':
@@ -20,10 +24,33 @@ def signup(request):
         else:
             form = UserCreation()
         
-    return render(request, 'authentication_app/signup.html', {'signup_form': form})
+    return render(request, 'authentication_app/sign-up.html', {'signup_form': form})
 
 def login(request):
-    return render(request, 'authentication_app/login.html')
+    form = UserLogin()
+    if request.method == 'POST':
+        form = UserLogin(data=request.POST)
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+                if user.roleType == 'admin':
+                    return redirect('admin-dashboard')
+                else:
+                    return redirect('dashboard')
+            else:
+                messages.info(request, 'Username OR Password is incorrect')
+                form = UserLogin()
+
+    return render(request, 'authentication_app/login.html', {'login_form': form})
+
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('sign')
 
 def dashboard(request):
     pass
