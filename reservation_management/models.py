6e8302cb768df84ae4c97e6cc334dae6cb93a509
GuriGhotra <prince1992ghotra.gs@gmail.com>
2024-04-29 19:26:37 +0100

    Finished the Back-end part

--- reservation_management/models.py
+++ reservation_management/models.py
@@ -8,5 +8,6 @@ class Reservation(models.Model):
     expectedReturnDate = models.DateTimeField()
     approvalStatus = models.CharField(max_length=100) # PROBABLY CHANGE THE VALUE TO BOOLEAN
     isCancelled = models.BooleanField(default=False)
+    quantity = models.IntegerField(default=False)
     equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
