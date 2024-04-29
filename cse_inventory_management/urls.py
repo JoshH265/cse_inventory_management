0bb4b0b180d2c27ada3891d78de45d2d0f0c60a8
CloverDragana <blaacklist@live.com>
2024-04-29 17:41:23 +0100

    changed nav and added drop down to some extra info inputs

--- cse_inventory_management/urls.py
+++ cse_inventory_management/urls.py
@@ -19,14 +19,12 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    path('', include('user_account.urls')),
-    path('', include('authentication_app.urls')),
-    path('', include('equipment_management.urls')),
-    path('', include('report_management.urls')),
+    path('user/', include('user_account.urls')),
+    path('auth/', include('authentication_app.urls')),
+    path('inventory/', include('equipment_management.urls')),
+    path('report/', include('report_management.urls')),
     path('', include('home.urls')),
-    path('', include('user_management.urls')),
-    path('', include('reservation_management.urls')),
-
+    path('user_management', include('user_management.urls')),
+    path('reservation_management', include('reservation_management.urls')),
 
 ]
