a3b4b50fa534b1b4758476772bc28b6fe1454012
rabsa784 <w1904911@my.westminster.ac.uk>
2024-04-29 12:15:20 +0100

    django attempt

--- user_account/models.py
+++ user_account/models.py
@@ -4,8 +4,8 @@ from reservation_management.models import Reservation
 
 class UserProfile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    bio = models.TextField(blank=True)
-    # Additional fields like address, phone number, etc., can be added here.
+    # bio = models.TextField(blank=True)
+    # # Additional fields like address, phone number, etc., can be added here.
 
     def current_reservations(self):
         return Reservation.objects.filter(user=self.user, isCancelled=False)
--- user_account/views.py
+++ user_account/views.py
@@ -1,4 +1,30 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from reservation_management.models import Reservation
+from user_account.models import UserProfile
 
 def user_account(request):
     return render(request, 'user_account/userprofile.html')
+
+# @login_required
+# def profile(request):
+#     user = request.user  # Retrieve the logged-in user
+#     return render(request, 'userprofile.html', {'user': user})
+
+# def reservation_table(request):
+#     reservations = Reservation.objects.filter(user = request.user)
+#     return render(request, 'userprofile.html', {'reservations': reservations})
+
+def reservation_table(request):
+    # Retrieve the user profile associated with the logged-in user
+    user_profile = UserProfile.objects.get(user=request.user)
+    
+    # Get current reservations for the user
+    current_reservations = user_profile.current_reservations()
+
+    # Optionally, you can also get reservation history
+    reservation_history = user_profile.reservation_history()
+
+    num_reservations = current_reservations.count()
+
+    return render(request, 'userprofile.html', {'current_reservations': current_reservations, 'reservation_history': reservation_history})
