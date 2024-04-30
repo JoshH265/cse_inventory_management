267938c245821468464ab99742524281ad41c77e
Josh <108278844+JoshH265@users.noreply.github.com>
2024-04-30 22:04:32 +0100

    Fixed issues with PR

--- reservation_management/urls.py
+++ reservation_management/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("adminReservation",views.adminManagement, name='adminReservation')
+    path("reservation_management",views.adminManagement, name='adminReservation')
 ]
 
