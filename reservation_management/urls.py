eab9cb306d85e454d53ab245a71ca4e9100c6f43
Josh <108278844+JoshH265@users.noreply.github.com>
2024-04-30 22:14:28 +0100

    test fix

--- reservation_management/urls.py
+++ reservation_management/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("reservation_management",views.adminManagement, name='adminReservation')
+    path("adminReservation",views.adminManagement, name='adminReservation')
 ]
 
